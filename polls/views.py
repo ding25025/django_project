8395695601511ba9fadf38538a913d78b67412a4
yutingwu <ding25025@gmail.com>
2022-04-09 12:30:29 +0800

    add fhir demo page

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+import requests
+import json
+
+headers = {'Content-Type': 'application/json'}
+fhirbaseURL = "https://fhir.dicom.tw/fhir/"
+
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+   
+    return HttpResponse("Hello~")
+
+def fhirdemo(request):
+    
+    r = requests.get(fhirbaseURL+"Patient", verify=False)
+    PatJson = r.json()["entry"]
+    for entry in PatJson:
+        print(entry['resource'])
+
+    return HttpResponse(len(PatJson))
